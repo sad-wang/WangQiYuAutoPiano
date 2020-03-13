abeea8a1f5cf3954d2499e1dca60069a40be8623
sad-wang <leown_wang@163.com>
2020-03-14 03:55:42 +0800

    added songNotation templete

--- src/components/Notation.vue
+++ src/components/Notation.vue
@@ -5,13 +5,25 @@
         <div class="pages"></div>
         <div class="page-left">
           <div class="content">
-            1 23 213 334252
+            <div style="color: #000;font-weight: bolder;padding-bottom: 10px">Mapping: real</div>
+            <span v-for="(node,index) in songbie" :key="index">
+              {{findKey(real, songbie[index])}}
+            </span>
           </div>
         </div>
       </div>
       <div class="right">
         <div class="page-right">
-          长亭外 古道边
+         <div class="content" style="height:100%;display:flex;flex-direction: column;justify-content: space-around;align-items: center;">
+           <div>长亭外 古道边</div>
+           <div>芳草碧连天</div>
+           <div>夕阳山外山
+           </div>
+           <div>天之涯 地之角</div>
+           <div>知交半零落</div>
+           <div>一瓢浊酒尽余欢</div>
+           <div>今宵别梦寒</div>
+         </div>
         </div>
       </div>
     </div>
@@ -20,7 +32,59 @@
 
 <script>
 export default {
-  name: 'Notation'
+  name: 'Notation',
+  data () {
+    return {
+      songbie: ['g5', 'e5', 'g5', 'c6', 'g5', 'e5', 'b5', 'a5', 'f5', 'c6', 'g5', 'e5', 'g5', 'e5', 'c5', 'd5', 'e5', 'c5', 'd5', 'c5', 'd5', 'b4', 'g4', 'b4', 'g5', 'e5', 'g5', 'c6', 'g5', 'e5', 'b5', 'a5', 'f5', 'c6', 'g5', 'e5', 'g5', 'd5', 'd5', 'e5', 'f5', 'b4', 'b4', 'c5', 'g4', 'e4', 'g4', 'as4', 'as4', 'f4', 'a4', 'c5', 'e4', 'g4', 'c5', 'd4', 'f4', 'b4', 'a4', 'b4', 'e4', 'g4', 'c5', 'f4', 'a4', 'b4', 'c5', 'a4', 'c4', 'f4', 'a4', 'g4', 'e4', 'c4', 'd4', 'b3', 'd4', 'f4', 'g4', 'e4', 'g4', 'e4', 'g4', 'c5', 'b4', 'f4', 'a4', 'c5', 'e4', 'g4', 'd4', 'g4', 'd4', 'e4', 'd4', 'f4', 'g3', 'c4', 'e4', 'a5', 'c6', 'c6', 'b5', 'a5', 'b5', 'c6', 'a5', 'b5', 'c6', 'a5', 'a5', 'g5', 'e5', 'c5', 'd5', 'g4', 'g5', 'e5', 'g5', 'c6', 'b5', 'a5', 'c6', 'g5', 'g5', 'd5', 'e5', 'f5', 'b4', 'e5', 'c5', 'g4'],
+      real: {
+        Tab: 'c3',
+        q: 'd3',
+        w: 'e3',
+        e: 'f3',
+        r: 'g3',
+        t: 'a3',
+        y: 'b3',
+        u: 'c4',
+        i: 'd4',
+        o: 'e4',
+        p: 'f4',
+        z: 'g4',
+        x: 'a4',
+        c: 'b4',
+        v: 'c5',
+        b: 'd5',
+        n: 'e5',
+        m: 'f5',
+        ',': 'g5',
+        '.': 'a5',
+        '/': 'b5',
+        Shift: 'c6',
+        1: 'cs3',
+        2: 'ds3',
+        4: 'fs3',
+        5: 'gs3',
+        6: 'as3',
+        8: 'cs4',
+        9: 'ds4',
+        a: 'fs4',
+        s: 'gs4',
+        d: 'as4',
+        g: 'cs5',
+        h: 'ds5',
+        k: 'fs5',
+        l: 'gs5',
+        ';': 'as5'
+      }
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    findKey (obj, value, compare = (a, b) => a === b) {
+      return Object.keys(obj).find(k => compare(obj[k], value))
+    }
+  }
 }
 </script>
 
@@ -60,6 +124,9 @@ export default {
         box-shadow: 3px 0 5px #868686;
         background-image: linear-gradient(to right,#f0f0f0 0%,#f1f1f1 10%,#d9d9d9 78%,#b1b1b1 100%);
         .content{
+          padding: 20px;
+          font-size: 16px;
+          color: gray;
         }
       }
     }
@@ -78,6 +145,11 @@ export default {
         height: 100%;
         box-shadow: 3px 0 5px #868686;
         background-image: linear-gradient(to right,#f8f8f8 0%,#fafafa 22%,#d4d4d4 90%,#c8c8c8 100%);
+        .content{
+          padding: 40px;
+          font-size: 16px;
+          color: gray;
+        }
       }
       .pages{
         width: 15px;
--- src/components/blackKey.vue
+++ src/components/blackKey.vue
@@ -4,7 +4,7 @@
 
 <script>
     export default {
-        name: "blackKey.vue"
+        name: 'blackKey.vue'
     }
 </script>
 
